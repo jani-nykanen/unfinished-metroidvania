144e2606ff63bb1c25e2094a8228df517a34a34a
jani-nykanen <jnyknn@gmail.com>
2021-04-05 11:42:44 +0300

    Bats

--- output/js/enemytypes.js
+++ output/js/enemytypes.js
@@ -1,8 +1,11 @@
 import { Flip } from "./core/canvas.js";
+import { clamp } from "./core/mathext.js";
 import { Vector2 } from "./core/vector.js";
 import { Enemy } from "./enemy.js";
-// Not very safe
-export const getEnemyType = (index) => [Slime][index];
+// If I make it a pure array, it will complain that "Slime" used
+// before declared
+const ENEMY_TYPES = () => [Slime, Bat];
+export const getEnemyType = (index) => ENEMY_TYPES()[clamp(index, 0, ENEMY_TYPES().length - 1) | 0];
 export class Slime extends Enemy {
     constructor(x, y) {
         super(x, y, 0, 3, 2);
@@ -42,3 +45,50 @@ export class Slime extends Enemy {
     }
 }
 Slime.JUMP_TIME = 60;
+export class Bat extends Enemy {
+    constructor(x, y) {
+        super(x, y, 1, 3, 2);
+        this.collisionBox = new Vector2(8, 8);
+        this.hitbox = new Vector2(8, 8);
+        // For "sleeping" effect
+        this.pos.y -= 4;
+        this.fallTimer = 0;
+        this.awake = false;
+        this.moveDir = new Vector2(0, 0);
+        this.friction = new Vector2(0.05, 0.05);
+    }
+    updateAI(ev) {
+        const ANIM_SPEED = 6.0;
+        const MOVE_SPEED = 0.5;
+        if (!this.awake)
+            return;
+        if (this.fallTimer > 0) {
+            this.spr.setFrame(1, this.spr.getRow());
+            this.fallTimer -= ev.step;
+            return;
+        }
+        this.spr.animate(this.spr.getRow(), 2, 5, ANIM_SPEED, ev.step);
+        this.target = Vector2.scalarMultiply(this.moveDir, MOVE_SPEED);
+    }
+    playerEvent(pl, ev) {
+        const WAKE_UP_DISTANCE = 32;
+        const DELTA_Y = 8;
+        const FALL_TIME = 30;
+        const FALL_SPEED = 1.0;
+        let p = pl.getPos();
+        if (this.awake) {
+            if (this.fallTimer <= 0) {
+                this.moveDir = Vector2.direction(this.pos, p);
+            }
+            return;
+        }
+        if (this.health < this.maxHealth ||
+            (p.y > this.pos.y + DELTA_Y &&
+                Math.abs(p.x - this.pos.x) < WAKE_UP_DISTANCE)) {
+            this.awake = true;
+            this.fallTimer = FALL_TIME;
+            this.speed.y = FALL_SPEED;
+            this.target.y = this.speed.y;
+        }
+    }
+}
--- src/enemytypes.ts
+++ src/enemytypes.ts
@@ -1,12 +1,17 @@
 import { Flip } from "./core/canvas.js";
 import { GameEvent } from "./core/core.js";
+import { clamp } from "./core/mathext.js";
 import { Vector2 } from "./core/vector.js";
 import { Enemy } from "./enemy.js";
+import { InteractionTargetWithCollisions } from "./interactiontarget.js";
 import { Player } from "./player.js";
 
 
-// Not very safe
-export const getEnemyType = (index : number) : Function => [Slime][index];
+// If I make it a pure array, it will complain that "Slime" used
+// before declared
+const ENEMY_TYPES = () : Array<Function> => [Slime, Bat];
+
+export const getEnemyType = (index : number) : Function => ENEMY_TYPES()[clamp(index, 0, ENEMY_TYPES().length-1) | 0];
 
 
 export class Slime extends Enemy {
@@ -75,3 +80,84 @@ export class Slime extends Enemy {
     }
 }
 
+
+
+export class Bat extends Enemy {
+
+
+    private awake : boolean;
+    private fallTimer : number;
+    private moveDir : Vector2;
+
+
+    constructor(x : number, y : number) {
+
+        super(x, y, 1, 3, 2);
+
+        this.collisionBox = new Vector2(8, 8);
+        this.hitbox = new Vector2(8, 8);
+
+        // For "sleeping" effect
+        this.pos.y -= 4;
+
+        this.fallTimer = 0;
+        this.awake = false;
+    
+        this.moveDir = new Vector2(0, 0);
+        this.friction = new Vector2(0.05, 0.05);
+    }
+
+
+    protected updateAI(ev : GameEvent) {
+
+        const ANIM_SPEED = 6.0;
+        const MOVE_SPEED = 0.5;
+
+        if (!this.awake) return;
+
+        if (this.fallTimer > 0) {
+
+            this.spr.setFrame(1, this.spr.getRow());
+            this.fallTimer -= ev.step;
+
+            return;
+        }
+
+        this.spr.animate(this.spr.getRow(), 2, 5, ANIM_SPEED, ev.step);
+        
+        this.target = Vector2.scalarMultiply(this.moveDir, MOVE_SPEED);
+    }
+
+
+    protected playerEvent(pl : Player, ev : GameEvent) {
+
+        const WAKE_UP_DISTANCE = 32;
+        const DELTA_Y = 8;
+        const FALL_TIME = 30;
+        const FALL_SPEED = 1.0;
+
+        let p = pl.getPos();
+
+        if (this.awake) {
+
+            if (this.fallTimer <= 0) {
+
+                this.moveDir = Vector2.direction(this.pos, p);
+            }
+
+            return;
+        }
+
+        if (this.health < this.maxHealth || 
+            (p.y > this.pos.y + DELTA_Y &&
+            Math.abs(p.x - this.pos.x) < WAKE_UP_DISTANCE)) {
+
+            this.awake = true;
+            this.fallTimer = FALL_TIME;
+
+            this.speed.y = FALL_SPEED;
+            this.target.y = this.speed.y;
+        }
+
+    }
+}
